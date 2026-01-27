0b3307f2a11d759f132ef806e515ac6c0ab13fa8
vanshbisht74-arch <vanshbisht74@gmail.com>
2026-01-27 15:24:24 +0530

    Add files via upload

---
+++ day3script.js
@@ -0,0 +1,27 @@
+// let arr=[3,6,8,9,0]
+// let a=[]
+// for(let i=0;i=arr.length;i++)
+// {
+// let apop=arr.pop();
+// a.push(apop);
+// }
+
+// console.log(a)
+// // let arr=[1,-1,2,-2,4,5]
+// // while(true);
+// // {
+
+// let arr=[3,-5,-6,7,8];
+// let pi=[]
+// while(arr.length!=0)
+// {
+//     let val=arr.shift();
+//     if (var>0)
+//     {
+//         pi.push(val)
+//     }
+// }
+// console.log(pi)
+arr=[7,8,9,10]
+arrs=[]
+for(let i=0;i<arr.lengthscop)
